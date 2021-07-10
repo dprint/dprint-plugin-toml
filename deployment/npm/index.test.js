36923116e17d172feeada6c15d89d17e82c7bbbc
David Sherret <dsherret@gmail.com>
2021-07-10 11:13:36 -0400

    chore: fix npm test

--- deployment/npm/index.test.js
+++ deployment/npm/index.test.js
@@ -4,6 +4,6 @@ const createFromBuffer = require("@dprint/formatter").createFromBuffer;
 const getBuffer = require("./index").getBuffer;
 
 const formatter = createFromBuffer(getBuffer());
-const result = formatter.formatText("file.toml", "key   =    value");
+const result = formatter.formatText("file.toml", "key   =   5");
 
-assert.strictEqual(result, "key = value\n");
+assert.strictEqual(result, "key = 5\n");
