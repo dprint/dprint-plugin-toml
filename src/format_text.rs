77e6449a215eb08440140cd349791c561177501b
David Sherret <dsherret@users.noreply.github.com>
2024-05-26 23:28:21 -0400

    fix: strip utf-8 bom (#17)

--- src/format_text.rs
+++ src/format_text.rs
@@ -10,9 +10,7 @@ use std::path::Path;
 use taplo::syntax::SyntaxNode;
 
 pub fn format_text(file_path: &Path, text: &str, config: &Configuration) -> Result<Option<String>> {
-  let node = parse_and_process_node(file_path, text, config)?;
-
-  let result = dprint_core::formatting::format(|| generate(node, text, config), config_to_print_options(text, config));
+  let result = format_text_inner(file_path, text, config)?;
   if result == text {
     Ok(None)
   } else {
@@ -20,11 +18,25 @@ pub fn format_text(file_path: &Path, text: &str, config: &Configuration) -> Resu
   }
 }
 
+fn format_text_inner(file_path: &Path, text: &str, config: &Configuration) -> Result<String> {
+  let text = strip_bom(text);
+  let node = parse_and_process_node(file_path, text, config)?;
+
+  Ok(dprint_core::formatting::format(
+    || generate(node, text, config),
+    config_to_print_options(text, config),
+  ))
+}
+
 #[cfg(feature = "tracing")]
 pub fn trace_file(file_path: &Path, text: &str, config: &Configuration) -> dprint_core::formatting::TracingResult {
   let node = parse_and_process_node(file_path, text, config).unwrap();
 
   dprint_core::formatting::trace_printing(|| generate(node, text, config), config_to_print_options(text, config))
+}
+
+fn strip_bom(text: &str) -> &str {
+  text.strip_prefix("\u{FEFF}").unwrap_or(text)
 }
 
 fn parse_and_process_node(file_path: &Path, text: &str, config: &Configuration) -> Result<SyntaxNode> {
@@ -56,5 +68,16 @@ fn config_to_print_options(text: &str, config: &Configuration) -> PrintOptions {
     max_width: config.line_width,
     use_tabs: config.use_tabs,
     new_line_text: resolve_new_line_kind(text, config.new_line_kind),
+  }
+}
+
+#[cfg(test)]
+mod test {
+  #[test]
+  fn strips_bom() {
+    let config = crate::configuration::ConfigurationBuilder::new().build();
+    let file_text = crate::format_text::format_text(&std::path::PathBuf::from("file.toml"), "\u{FEFF}# 1\n# 2\n", &config).unwrap();
+
+    assert_eq!(file_text.unwrap(), "# 1\n# 2\n");
   }
 }
